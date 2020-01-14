b79148304cb7c9f1d0c04e72c00bae7057381617
linqueta <lincolnrodrs@gmail.com>
2020-01-13 22:09:01 -0300

    recognize face

---
+++ face_recognizer.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+s3 = boto3.resource('s3')
+bucket_name = 'linqueta-fa-images'
+collection_name = 'faces'
+client = boto3.client('rekognition', region_name='us-east-1')
+
+def recognize():
+    recognized_faces = client.index_faces(
+        CollectionId=collection_name,
+        DetectionAttributes=['DEFAULT'],
+        ExternalImageId='TEMPORARY',
+        Image={
+            'S3Object': {
+                'Bucket': bucket_name,
+                'Name': 'drake-jeni.png',
+            },
+        },
+    )
+    return recognized_faces
+
+def main():
+    print(json.dumps(recognize(), indent=4))
+
+if __name__ == "__main__":
+    main()
